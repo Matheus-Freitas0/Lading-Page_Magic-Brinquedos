b338569d0f527a82e5e1574c8ed7fd82c38f0f2b
Matheus-Freitas0 <moncaomatheus@gmail.com>
2025-08-06 19:58:48 -0300

    Implementando bibliotecas necessarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>Magic Brinquedos</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Magic Brinquedos</h1>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/theme/index.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Lexend Deca', sans-serif`,
+  },
+  palette: {
+  },
+  components: {
+  },
+});
+
+export default theme;
